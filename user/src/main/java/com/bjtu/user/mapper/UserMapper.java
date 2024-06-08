b64039f617178035a137dba99f6070b6a1e79749
zycccccccccccccc <128199323+zycccccccccccccc@users.noreply.github.com>
2024-06-08 13:48:12 +0800

    Delete user directory

--- user/src/main/java/com/bjtu/user/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.bjtu.user.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.bjtu.commons.po.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
-
