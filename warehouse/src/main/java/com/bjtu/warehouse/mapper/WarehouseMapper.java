60b94530a6231d6dc1bf2d51e154e427a3b54582
zycccccccccccccc <128199323+zycccccccccccccc@users.noreply.github.com>
2024-06-08 13:48:21 +0800

    Delete warehouse directory

--- warehouse/src/main/java/com/bjtu/warehouse/mapper/WarehouseMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bjtu.warehouse.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.bjtu.commons.po.User;
-import com.bjtu.commons.po.Warehouse;
-
-public interface WarehouseMapper extends BaseMapper<Warehouse> {
-}
-
