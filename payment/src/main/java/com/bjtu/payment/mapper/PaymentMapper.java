f2c63dda531a63130217bfc187bc72225d5cbe8d
zycccccccccccccc <128199323+zycccccccccccccc@users.noreply.github.com>
2024-06-08 13:47:50 +0800

    Delete payment directory

--- payment/src/main/java/com/bjtu/payment/mapper/PaymentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bjtu.payment.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.bjtu.commons.po.Payment;
-import com.bjtu.commons.po.User;
-
-public interface PaymentMapper extends BaseMapper<Payment> {
-}
-
